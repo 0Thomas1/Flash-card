3d27f2121fa3bf2a9f2fac83adf6147c89b12390
0Thomas1 <99365833+0Thomas1@users.noreply.github.com>
2022-03-11 21:55:19 -0800

    flashcard
    hehexd

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+flash card 
+2022/03/10
+Create a flash card system for learning and memorizing
+
+Hiractchy :
+course > chapter > flash cards
+"""
+def load_flashcards():
+     
+
+def create_subject(subject):
+
+    subject = str(input("What is your subject?"))
+    subject_list.append(subject)
+    print(subject_list)
+    
+create_subject("English")
